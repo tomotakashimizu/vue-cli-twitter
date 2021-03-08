f5e7de19ca8f14915d396e194a2807d3b9f1150c
tomotakashimizu <tomotaka.developer@gmail.com>
2021-03-09 00:54:39 +0900

    Googleアカウントでログインするコード追記

--- src/views/MyPage.vue
+++ src/views/MyPage.vue
@@ -1,5 +1,19 @@
 <template>
   <div class="my-page">
     <h1>This is my page</h1>
+    <button v-on:click="signIn">サインインする</button>
   </div>
 </template>
+
+<script>
+import firebase from "firebase"
+
+export default {
+  methods: {
+    signIn() {
+      var provider = new firebase.auth.GoogleAuthProvider()
+      firebase.auth().signInWithRedirect(provider)
+    },
+  },
+}
+</script>
